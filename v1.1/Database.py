bea5c1e1ecf1db635ebc1cffb119b9be0272886d
Amin <mavericane@gmail.com>
2023-09-04 20:23:17 +0330

    Added v1.1

---
+++ v1.1/Database.py
@@ -0,0 +1,8 @@
+# Author: Amin (Mavericane)
+# Github Link: https://github.com/mavericane/
+# Website Link: https://mavericane.ir
+# Description: This file created by Create-Database.py to be used in Fetch_Used_Cars.py
+database_user = "username"
+database_pass = "password"
+database_name = "truecar"
+table_name = "used_cars"
